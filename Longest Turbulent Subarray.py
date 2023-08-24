7461ef5b0d2f743f26950404e7e5be0a82040cbd
soikat1139 <106826559+soikat1139@users.noreply.github.com>
2023-08-24 11:04:34 +0600

    Create Longest Turbulent Subarray.py

---
+++ Longest Turbulent Subarray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        l,r=0,1
+
+        res=1
+
+        prev=""
+
+        while r<len(arr):
+
+            if arr[r-1]>arr[r] and prev!=">":
+                res=max(res,r-l+1)
+                r+=1
+                prev=">"
+            elif arr[r-1]<arr[r] and prev!="<":
+
+                res=max(res,r-l+1)
+                r+=1
+                prev="<"
+            else:
+                r=r+1 if arr[r-1]==arr[r] else r
+                l=r-1
+                prev=""
+        return res

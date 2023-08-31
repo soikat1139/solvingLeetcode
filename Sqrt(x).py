0b6fdbfd239517f323a14e6e17e3c2de8e2d7d96
soikat1139 <106826559+soikat1139@users.noreply.github.com>
2023-08-31 10:11:26 +0600

    Create Sqrt(x).py

---
+++ Sqrt(x).py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l,r=0,x
+        res=0
+
+        while l<=r:
+            m=l+((r-l)//2)
+
+            if m*m >x:
+                r=m-1
+            elif m*m<x:
+                l=m+1
+                res=m
+            else:
+                return m
+        
+        return res

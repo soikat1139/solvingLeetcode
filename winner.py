7ae697569695d8384946636b7106c114a5ed5143
soikat1139 <106826559+soikat1139@users.noreply.github.com>
2023-10-02 08:53:02 +0600

    Create winner.py

---
+++ winner.py
@@ -0,0 +1,17 @@
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+
+        alice,bob=0,0
+
+        for i in range(1,len(colors)-1):
+
+            if colors[i-1]==colors[i]==colors[i+1]:
+
+                if colors[i]=="A":
+                    alice+=1
+                if colors[i]=="B":
+                    bob+=1
+        return alice>bob
+            
+
+        

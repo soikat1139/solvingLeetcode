5b2c9ef2b539e569442ae78253cefe9b9dab0f97
soikat1139 <106826559+soikat1139@users.noreply.github.com>
2023-10-05 14:02:05 +0600

    Create Split Array Largest Sum.py

---
+++ Split Array Largest Sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        def feasible(threshold) -> bool:
+            count = 1
+            total = 0
+            for num in nums:
+                total += num
+                if total > threshold:
+                    total = num
+                    count += 1
+                    if count > k:
+                        return False
+            return True
+
+        left, right = max(nums), sum(nums)
+        while left < right:
+            mid = left + (right - left) // 2
+            if feasible(mid):
+                right = mid     
+            else:
+                left = mid + 1
+        return left        
